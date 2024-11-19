a368338a1bb99349186055c749b196882f549e46
xml-aztec <asankozhoevn@gmail.com>
2024-11-19 22:47:01 +0600

    Обновить заголовок приложения и улучшить компонент ProductCard с обработкой ошибок, загрузкой и новыми стилями

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import { ProductCard } from "./components/productCard";
 function App() {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>setProducts(json))
-            .finally(()=>setLoading(false));
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then(json => setProducts(json))
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
-    <div>
-      {loading && <div>Loading...</div>}
+    <div className="container">
+      {loading && <img className="loading" src="https://i.gifer.com/ZKZg.gif"></img>}
+      {error && <div>Error: {error}</div>}
       {products.map((product) => 
-        (< ProductCard data={product} />))}
+        (<ProductCard key={product.id} data={product} />))}
     </div>
   );
 }
--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,14 +1,30 @@
 import "./index.css";
+
 export function ProductCard(props) {
-  
-  const {image, category, title, price} = props.data;
-  
+  const { image, category, title, price } = props.data;
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
   return (
-    <div className="product-card">
-      <img width={200} src={image} alt="Iphone 15 Pro Max" />
-      <div>{category}</div>
-      <h4>{title}</h4>
-      <div>$ {price}</div>
-    </div>
+      <div className="product-card">
+
+        <img width={200} height={250} src={image} alt="Iphone 15 Pro Max" />  
+        
+
+        <div className="category">{capitalizeFirstLetter(category)}</div>
+        <h4 className="title-card">{title}</h4>
+          
+        <div className="button-block">
+
+          <div className="price-card">${price}</div>
+          <button className="add-card">Add to card</button>
+
+        </div>
+
+      </div>
+
+
   );
 }
