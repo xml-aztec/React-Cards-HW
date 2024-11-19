e8fd4c09baa99dc5d002ec87d7bd054bb4c64096
xml-aztec <asankozhoevn@gmail.com>
2024-11-19 10:49:29 +0600

    Изменить компонент App для загрузки продуктов с API и обновить компонент ProductCard для отображения новых данных

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
+import { useEffect, useState } from "react";
 import { ProductCard } from "./components/productCard";
 
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    price: 100,
-    category: "Category 1",
-    img: "https://www.istore.co.za/media/catalog/product/cache/4ebc814e413626645aa42e369230a31f/i/p/iphone_16_pro_desert_titanium_pdp_image_position_1__gben_2.png",
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    price: 200,
-    category: "Category 2",
-    img: "https://www.istore.co.za/media/catalog/product/cache/4ebc814e413626645aa42e369230a31f/i/p/iphone_16_pro_desert_titanium_pdp_image_position_1__gben_2.png",
-  },
-  {
-    id: 3,
-    name: "Product 3",
-    price: 300,
-    category: "Category 3",
-    img: "https://www.istore.co.za/media/catalog/product/cache/4ebc814e413626645aa42e369230a31f/i/p/iphone_16_pro_desert_titanium_pdp_image_position_1__gben_2.png",
-  },
-]
-
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .finally(()=>setLoading(false));
+  }, []);
+
   return (
     <div>
+      {loading && <div>Loading...</div>}
       {products.map((product) => 
         (< ProductCard data={product} />))}
     </div>
--- src/components/productCard/index.jsx
+++ src/components/productCard/index.jsx
@@ -1,14 +1,14 @@
 import "./index.css";
 export function ProductCard(props) {
   
-  const {img, category, name, price} = props.data;
+  const {image, category, title, price} = props.data;
   
   return (
     <div className="product-card">
-      <img width={200} src={img} alt="Iphone 15 Pro Max" />
+      <img width={200} src={image} alt="Iphone 15 Pro Max" />
       <div>{category}</div>
-      <h4>{name}</h4>
-      <div>{price}</div>
+      <h4>{title}</h4>
+      <div>$ {price}</div>
     </div>
   );
 }
